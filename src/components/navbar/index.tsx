22490b5b0b31554574b4402917f3d6c28da9970f
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2025-10-01 21:56:35 -0300

    ajusta o padding da navbar para melhorar a responsividade

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -18,7 +18,7 @@ export function NavBar() {
   return (
     // border-b-1 border-border
     <nav className="text-gold-100 font-montserrat fixed top-0 left-0 right-0 z-50 bg-background/80 backdrop-blur-md">
-      <div className="container mx-auto px-4">
+      <div className="container mx-auto px-8">
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
           <div className="flex items-center space-x-2">
